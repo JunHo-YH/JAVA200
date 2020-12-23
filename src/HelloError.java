5e2ddf04f4d8fa879ed553ff8f449a332426e9d8
강준호 <@DESKTOP-NGK7J5N>
2020-12-23 23:06:31 +0900

    Because of non-semiclone

---
+++ src/HelloError.java
@@ -0,0 +1,9 @@
+// 화면에 출력하는 프로그램(오류: 세미클론 누락)
+public class HelloError {
+
+	public static void main(String[] args) {
+		System.out.println("첫 Java 프로그램입니다.")
+		System.out.println("화면에 출력하고 있습니다.");
+	}
+
+}
