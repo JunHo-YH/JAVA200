efdf99f1893eace1a3144731d747f264e7ad1130
강준호 <@DESKTOP-NGK7J5N>
2020-12-24 20:21:49 +0900

    +

---
+++ src/chapter2/SumDiff.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class SumDiff {
+
+	public static void main(String[] args) {
+		System.out.println(82 + 17);
+		System.out.println(82 - 17);
+	}
+
+}
---
+++ src/chapter2/SumDiff2.java
@@ -0,0 +1,10 @@
+package chapter2;
+
+public class SumDiff2 {
+
+	public static void main(String[] args) {
+		System.out.println("82 + 17 = " + (82 + 17));
+		System.out.println("82 - 17 = " + (82 - 17));
+	}
+
+}
