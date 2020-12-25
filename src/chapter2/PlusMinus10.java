f7931ce87715dd1bb71ff19c87a80f1aa4c0c79b
강준호 <@DESKTOP-NGK7J5N>
2020-12-25 22:27:49 +0900

    Scanner

---
+++ src/chapter2/PlusMinus10.java
@@ -0,0 +1,16 @@
+package chapter2;
+
+import java.util.Scanner;
+public class PlusMinus10 {
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		
+		System.out.print("정숫값: ");
+		int x = scan.nextInt(); // x에 입력받은 정숫값을 저장한다.
+		
+		System.out.printf("10을 더한 값은 %d입니다,\n", x+10);
+		System.out.printf("10을 뺀 값은 %d입니다.", x-10);
+	}
+
+}
