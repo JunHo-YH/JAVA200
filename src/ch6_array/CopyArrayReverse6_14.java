8d55ac039c005072f6bea9056d7c38da92eece04
JunHo <u_innovation@naver.com>
2021-01-10 09:14:49 +0900

    배열 역순으로 출력하기 복습

--- src/ch6_array/CopyArrayReverse6_14.java
+++ src/ch6_array/CopyArrayReverse6_14.java
@@ -7,27 +7,26 @@ public class CopyArrayReverse6_14 {
 	public static void main(String[] args) {
 		Scanner scan = new Scanner(System.in);
 		
-		System.out.print("요소 수:");
+		System.out.println("몇 개의 배열을 생성? ");
 		int n = scan.nextInt();
+		
 		int[] arr1 = new int[n];
 		int[] arr2 = new int[n];
 		
-		for (int i = 0; i < n; i++) {
-			System.out.print("a[" + i + "] = ");
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.print("arr1[" + i + "] = ");
 			arr1[i] = scan.nextInt();
 		}
 		
-		// 배열 arr1의 모든 요소를 역순으로 배열 arr2에 복사
-		for (int i = 0; i < n; i++) {
-			arr2[i] = arr1[n - i - 1];
+		for (int k = 0; k < arr1.length; k++) {
+			arr2[k] = arr1[n-1-k];
 		}
 		
-		System.out.println("arr1의 모든 요소를 역순으로 복사했습니다.");
-		
-		for (int i = 0; i < n; i++) {
-			System.out.println("b[" + i + "] = " + arr1[i]);
+	
+		System.out.println("====================");
+		for (int i = 0; i < arr1.length; i++) {
+			System.out.println("arr2[" + i + "] = " + arr2[i]);
 		}
-		
 		
 	}
 
