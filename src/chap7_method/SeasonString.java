11e8258ad594427df3503faf3a0c818ec35481d9
JunHo <u_innovation@naver.com>
2021-01-13 23:31:27 +0900

    return값을 잘 계산해서 메서드를 작성하자

--- src/chap7_method/SeasonString.java
+++ src/chap7_method/SeasonString.java
@@ -6,10 +6,10 @@ public class SeasonString {
 	// m월의 계절을 표시
 	static String seasonOf(int m) {
 		switch(m) {
-			case 3: case 4: case 5: System.out.print("봄"); break;
-			case 6: case 7: case 8: System.out.print("여름"); break;
-			case 9: case 10: case 11: System.out.print("가을"); break;
-			case 1: case 2: case 12: "겨울"; break;
+			case 3: case 4: case 5: return "봄"; 
+			case 6: case 7: case 8: return "여름"; 
+			case 9: case 10: case 11: return "가을"; 
+			case 1: case 2: case 12: return "겨울"; 
 		}
 		return ""; // m이 1~12가 아니면 공백 문자열을 반환
 	}
